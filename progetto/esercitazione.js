994add3329cabea15503f7037df9e01247dd60c9
amm <amm@amm-VirtualBox>
2015-05-20 18:08:39 +0200

    prima versione progetto

---
+++ progetto/esercitazione.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var hello = "Hello";
+var nome = "Riccardo";
+
+document.write("<b>" +hello+ " " +nome+ "</b>");
+
+var variabile;
+
+variabile = 8;
+document.write("Assegno 8, variabile" +variabile+ "</br>");
+
+variabile += 2;
+document.write("Sommo 2, variabile" +variabile+ "</br>");
+
+variabile -= 4;
+document.write("Sottraggo 4, variabile" +variabile+ "</br>");
+
+variabile += 5;
+document.write("Moltiplico per 5, variabile" +variabile+ "</br>");
+
+variabile /= 3;
+document.write("Divido per 3, variabile" +variabile+ "</br>");
+
+
+
