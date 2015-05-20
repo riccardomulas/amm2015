4c462f0c16513d64782cfe80c45e225910524bfe
amm <amm@amm-VirtualBox>
2015-05-20 18:23:47 +0200

    prima versione progetto

--- progetto/esercitazione.js
+++ progetto/esercitazione.js
@@ -26,5 +26,27 @@ document.write("Moltiplico per 5, variabile" +variabile+ "</br>");
 variabile /= 3;
 document.write("Divido per 3, variabile" +variabile+ "</br>");
 
+var date = new Date();
+var m = date.getMonth();
 
+if(m==2){
+    document.write("<i>Siamo a marzo!</i>");
+}
 
+var mesi = new Array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre");
+
+var months = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+document.write("<ol>")
+for(var i in mesi){
+    document.write("<li>"+mesi[i]+ "corrisponde a "+months[i]+"</li>");
+}
+document.write("</ol>");
+
+var undef;
+
+if(undef==undefined){
+    document.write("<h3> La variabile non è definita</h3>");
+} else{
+    document.write("<h3> La variabile è definita</h3>");
+}
